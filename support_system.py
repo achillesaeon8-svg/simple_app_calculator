c796eacfe0157bcb641b48e16638f4dba631c68e
unknown <achillesaeon8-svg>
2026-05-08 19:40:04 +0800

    Smart Entry Class File Draft

--- support_system.py
+++ support_system.py
@@ -1,5 +1,8 @@
 class SmartEntry:
-  pass
+  @staticmethod
+  def refine_input(user_text):
+    return user_text.replace('++', '+').replace('--', '-').replace('xx', '*')
+
 class FactionModification:
   pass
 class DecimalModification:
