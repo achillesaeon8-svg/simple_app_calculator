0d3badcb7bb51ca3ad928b142f959730a96557ca
Achilles Mallari <achillesaeon8@gmail.com>
2026-05-08 13:31:57 +0800

    Simple App Calculator, Calculator Library

--- calculator_library.py
+++ calculator_library.py
@@ -1,2 +1,8 @@
 class InputHistory:
-  pass
+  def __init__(self):
+    self.calculation_logs = []
+  
+  def save_to_library(self, math_entry):
+    self.calculation_logs.append(math_entry)
+    with open ('calculation_history.txt', 'a') as history_file:
+      history_file.write(math_entry + '\n')
