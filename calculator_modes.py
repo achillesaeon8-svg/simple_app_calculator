77aa3dcafc7a76421afe1e6108028e822e0f2a82
unknown <achillesaeon8-svg>
2026-05-08 19:56:46 +0800

    Calculator Modes Class Variables Added for Inheritance Implementation

--- calculator_modes.py
+++ calculator_modes.py
@@ -1,17 +1,17 @@
 from calculator_settings import FundamentalOperations
 
-class AdvancedMath:
+class AdvancedMath(FundamentalOperations):
   def __init__(self):
     super().__init__(window_title='Advanced Mode', background_color='8e44ad')
 
-class TimeCalculator:
+class TimeCalculator(FundamentalOperations):
   def __init__(self):
     super().__init__(window_title='Advanced Mode', background_color='8e44ad')
 
-class UnitConverter:
+class UnitConverter(FundamentalOperations):
   def __init__(self):
     super().__init__(window_title='Advanced Mode', background_color='8e44ad')
-    
-class CurrencyConverter:
+
+class CurrencyConverter(FundamentalOperations):
   def __init__(self):
     super().__init__(window_title='Advanced Mode', background_color='8e44ad')
