75d66d2bd32204b81a747125d3abd0bb3be4acae
unknown <achillesaeon8-svg>
2026-05-08 19:27:19 +0800

    Roman Quotes Added

--- additional_features.py
+++ additional_features.py
@@ -1,5 +1,29 @@
 class DailyQuotes:
-  pass
+  @staticmethod
+  def get_random_quote():
+    quote_list = [
+      'Veni, vidi, vici.',
+      'Et tu, Brute?',
+      'The best revenge is to be unlike him who performed the injury.',
+      'Carthago delenda est.',
+      'Fortune favors the bold.',
+      'While they teach, they learn.',
+      'In wine, there is truth.',
+      'As is a tale, so is life: not how long it is, but how good it is, is what matters.',
+      'I found Rome a city of bricks and left it a city of marble.',
+      'Brave men are a city\'s strongest tower of defense.',
+      'If you want peace, prepare for war.',
+      'To err is human.',
+      'Waste no more time arguing what a good man should be. Be one.',
+      'Laws are silent in times of war.',
+      'Endurance is the crowning quality.',
+      'A room without books is like a body without a soul.',
+      'Bread and circuses.',
+      'Carpe diem.',
+      'Love conquers all things; let us too surrender to love.',
+      'History is the teacher of life.',
+    ]
+    
 class MultiColor:
   pass
 class SoundBoard:
