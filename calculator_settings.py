efdbb5081f66ecd1ef40424d48b8824838cec57b
Achilles Mallari <achillesaeon8@gmail.com>
2026-05-08 13:06:38 +0800

    Simple App Calculator Caculato Settings Class File Draft

--- calculator_settings.py
+++ calculator_settings.py
@@ -15,3 +15,18 @@ class FundamentalOperations(tkinter_library.TK):
         bd = 10
         background = '#ecf0f1'
     )
+    self.main_display.pack(fill='x', padx=10, pady=20)
+    t
+    def execute_safe_calculation(self):
+        try:
+            input_string = self.main_display.get()
+            calculation_result = eval(input_string, {'__builtins__': None}, vars(math))
+            
+            self.main_display.delete(0, tkinter_library.END)
+            self.main_display.insert(0, str(calculation_result))
+            return input_string, calculation_result
+        except ZeroDivisionError:
+            messagebox.showerror('Math Error', 'Cannot divide by zero.')
+        except Exception as ErrorMessage:
+            messagebox.showerror('Error', f'invalid input: {error_message}')
+            return None, None
