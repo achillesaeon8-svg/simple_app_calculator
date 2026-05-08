11cdd5a1fc61b9d9d3df3bbc6a63fd28bf3352fb
unknown <achillesaeon8-svg>
2026-05-08 20:04:06 +0800

    Additional Imports of Class Files in Main Launcher and Preparation for the function of a Simple App Calculator

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from calculator_modes import AdvancedMath
 from additional_features import DailyQuotes, SoundBoard
 from calculator_library import InputHistory
+from support_system import SmartEntry
 from tkinter import messagebox
 import tkinter as tkinter_library
 
+def run_calculator_app():
+    print(f'-----Welcome: {DailyQuotes.get_random_quote()} -----')
+
+    active_calculator = AdvancedMath()
+    session_history = InputHistory()
+
+    def handle_calculation_process():
+        SoundBoard.trigger_click_sound()
+
+        raw_input = active_calculator.main_display.get()
+        cleaned_input = Smart
