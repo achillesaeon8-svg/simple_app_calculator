a484ab516bec1aaac058508f9a62b2bf63c94146
unknown <achillesaeon8-svg>
2026-05-08 20:25:39 +0800

    Unsuccessful saved re-pushing again

--- calculator_modes.py
+++ calculator_modes.py
@@ -2,16 +2,16 @@ from calculator_settings import FundamentalOperations
 
 class AdvancedMath(FundamentalOperations):
   def __init__(self):
-    super().__init__(window_title='Advanced Mode', background_color='8e44ad')
+    super().__init__(window_title='Advanced Mode', background_color='#8e44ad')
 
 class TimeCalculator(FundamentalOperations):
   def __init__(self):
-    super().__init__(window_title='Advanced Mode', background_color='8e44ad')
+    super().__init__(window_title='Advanced Mode', background_color='#8e44ad')
 
 class UnitConverter(FundamentalOperations):
   def __init__(self):
-    super().__init__(window_title='Advanced Mode', background_color='8e44ad')
+    super().__init__(window_title='Advanced Mode', background_color='#8e44ad')
 
 class CurrencyConverter(FundamentalOperations):
   def __init__(self):
-    super().__init__(window_title='Advanced Mode', background_color='8e44ad')
+    super().__init__(window_title='Advanced Mode', background_color='#8e44ad')
